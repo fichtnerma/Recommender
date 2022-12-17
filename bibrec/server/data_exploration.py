a6956b60ece026e3c227759fdbc9935509e2573b
Fabian Untermoser <funtermoser.its-b2019@fh-salzburg.ac.at>
2022-12-17 11:37:19 +0100

    format(data): remove dead code, avoid resetting default arguments

--- bibrec/server/data_exploration.py
+++ bibrec/server/data_exploration.py
@@ -1,5 +1,90 @@
 from Utils import get_books, get_users, get_ratings
 import pandas as pd
+import numpy as np
+
+
+def get_books(path="../../data/BX-Books.csv"):
+    books = pd.read_csv(path, sep=";", encoding="latin-1")
+    books.columns = books.columns.map(prepare_string)
+    books.year_of_publication = pd.to_numeric(books.year_of_publication, errors='coerce')
+    # Replace years above 2005 with NaN (dataset was released in 2005)
+    books.loc[books.year_of_publication > 2005, 'year_of_publication'] = 0
+    mean_year_of_publication = round(books.year_of_publication.mean())
+    # replace invalid years with mean
+    books.year_of_publication.replace(0, mean_year_of_publication, inplace=True)
+    # Replace NaN years with mean of all years
+    books["isbn13"] = books.isbn.map(convert_isbn)
+    books = books[books.isbn13.notna()]
+
+    return books
+
+
+def get_users(path="../../data/BX-Users.csv"):
+    users = pd.read_csv(path, sep=";", encoding="latin-1")
+    # cleaned column names
+    users.columns = users.columns.map(prepare_string)
+    # replaced ages below 6 and above 110 with NaN
+    users.loc[(users.age < 6) | (users.age > 110), 'age'] = np.nan
+    print("With NaN values", users.age.mean())
+    # replaced NaN ages with random ages from normal distribution
+    temp_age_series = pd.Series(
+        np.random.normal(loc=users.age.mean(), scale=users.age.std(), size=users.user_id[users.age.isna()].count()))
+    pos_age_series = np.abs(temp_age_series)
+    users = users.sort_values('age', na_position='first').reset_index(drop=True)
+    users.age.fillna(pos_age_series, inplace=True)
+    users = users.sort_values('user_id').reset_index(drop=True)
+    print("used mean values", users.age.mean())
+    # separate location into city, state and country
+    location_seperated = users.location.str.split(',', 2, expand=True)
+    location_seperated.columns = ['city', 'state', 'country']
+    users = users.join(location_seperated)
+    users.drop(columns=['location'], inplace=True)
+    # replaced empty strings with NaN
+    users.country.replace('', np.nan, inplace=True)
+    users.state.replace('', np.nan, inplace=True)
+    users.city.replace('', np.nan, inplace=True)
+
+    return users
+
+
+def get_ratings(path="../../data/BX-Book-Ratings.csv"):
+    ratings = pd.read_csv(path, sep=";", encoding="latin-1")
+    ratings.columns = ratings.columns.map(prepare_string)
+    ratings["isbn13"] = ratings.isbn.map(convert_isbn)
+    ratings = ratings[ratings.isbn13.notna()]
+
+    return ratings
+
+
+def prepare_string(string):
+    return str(string).strip().lower().replace('-', '_')
+
+
+def convert_isbn(id):
+    isbn = str(id)
+    if len(isbn) == 10:
+        isbn = isbn.rstrip(isbn[-1])
+        isbn = "978" + isbn
+        sum = 0
+        for i in range(len(isbn)):
+            try:
+                int(isbn[i])
+            except ValueError:
+                return np.nan
+            if i % 2 == 0:
+                sum += int(isbn[i])
+            else:
+                sum += int(isbn[i]) * 3
+        check_digit = 10 - (sum % 10)
+        if check_digit == 10:
+            check_digit = 0
+        isbn += str(check_digit)
+        return isbn
+    elif len(isbn) == 13:
+        return isbn
+    else:
+        return np.nan
+>>>>>>> 3915f04 (format(data): remove dead code, avoid resetting default arguments)
 
 
 # filtern von ratings, die nicht in books sind
@@ -13,7 +98,6 @@ def add_mean_and_count(df, ratings):
     books_with_mean = df.merge(book_avg_rating, on='isbn13', how='left')
     books_with_mean.rename(columns={'mean': 'rating_mean', 'count': 'rating_count'}, inplace=True)
     books_with_mean["rating_count"].fillna(0, inplace=True)
-    books_with_mean["rating_mean"].fillna(0, inplace=True)
     return books_with_mean
 
 # add user mean and count to users
@@ -96,11 +180,11 @@ def get_normalized_data():
 
     books = add_mean_and_count(books, filtered_ratings)
     books = normalize_year_of_publication(books)
-    encoded_books = hot_encode_publisher(books, 10)
+    encoded_books = hot_encode_publisher(books)
 
     users = add_user_mean_and_count(users, filtered_ratings)
-    encoded_users = hot_encode_country(users, 10)
-    encoded_users = hot_encode_state(encoded_users, 10)
+    encoded_users = hot_encode_country(users)
+    encoded_users = hot_encode_state(encoded_users)
 
     normalized_ratings = normalize_ratings_for_user(filtered_ratings, users)
 
@@ -112,26 +196,5 @@ def get_lowest_rated_books(books, ratings, n=10):
     lowest_rated_books = lowest_rated_books[:n]
     lowest_rated_books = lowest_rated_books.reset_index()
     lowest_rated_books = lowest_rated_books.merge(books, on='isbn')
+
     return lowest_rated_books
-
-# get highest rated books in your country
-def get_highest_rated_books_in_country(books, ratings, country, n=10):
-    get_highest_rated_books(books, ratings, n=100)
-
-books, users, ratings = get_normalized_data()
-
-
-# import json
-
-# booksCopy = books
-
-# json_data = None
-# with open('../../data/ol_dump_editions_2010-12-31.json') as f:
-#     json_data = json.load(f.read())
-
-# editions_data = pd.json_normalize(json_data)
-
-
-# editions_data = editions_data[books['isbn'].isin(books['isbn'])]
-
-# editions_data.drop(["by_statement",""])
