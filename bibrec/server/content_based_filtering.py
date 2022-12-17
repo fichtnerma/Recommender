e1ea46cabcbdccd91b6fbfe645bff2d2451da016
Markus Fichtner <markus.fichtner1999@gmail.com>
2022-12-17 18:47:31 +0100

    predict in chunks

--- bibrec/server/content_based_filtering.py
+++ bibrec/server/content_based_filtering.py
@@ -1,56 +1,65 @@
 from Utils import *
-from sklearn.metrics.pairwise import cosine_similarity
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import linear_kernel
 
 books = get_books()
-
-bookData = pd.read_csv("../../data/editions_dump.csv", sep=",", encoding="utf-8")
-
-print(books.count())
-
-bookData.rename(columns={'isbn_10': 'isbn', 'first_sentence.value': 'first_sentence', 'notes.value': 'notes'}, inplace=True)
-
-print(bookData.head())
-print(books.head())
-
-bookData.isbn = bookData.isbn.map(lambda x: str(x).strip()[2:-2])
-
-print(bookData.isbn)
-
-bookData = books.merge(bookData, on="isbn", how="inner")
-
-bookData = bookData.drop(columns=["Unnamed: 0.1", "Unnamed: 0", "isbn_13", "publishers", "title", "first_sentence.type", "notes.type"])
-
-print(bookData.count())
-
-bookData = bookData.fillna(" ")
-
-bookData["book_info"] = bookData["book_title"] + " "+ bookData["subtitle"] + " " + bookData["subjects"] + " " + bookData["notes"] + " " + bookData["first_sentence"] + " " + bookData["genres"]
-
+CHUNK_QUANTITY = 5
 tfidf = TfidfVectorizer(stop_words='english')
-overview_matrix = tfidf.fit_transform(bookData["book_info"])
-#Output the shape of tfidf_matrix
-overview_matrix.shape
+bookData = pd.read_csv("../../data/editions_dump.csv", sep=",", encoding="utf-8")
+bookData.rename(columns={'isbn_10': 'isbn', 'first_sentence.value': 'first_sentence', 'notes.value': 'notes'}, inplace=True)
+bookData.isbn = bookData.isbn.map(lambda x: str(x).strip()[2:-2])
+bookData = books.merge(bookData, on="isbn", how="inner")
+bookData = bookData.drop(columns=["Unnamed: 0", "isbn_13", "publishers", "title", "first_sentence.type", "notes.type"])
+bookData.subjects = bookData.subjects.map(lambda x: str(x).strip()[2:-2])
+bookData.genres = bookData.genres.map(lambda x: str(x).strip()[2:-2])
+bookData = bookData.fillna(" ")
+bookData["book_info"] = bookData["book_title"] + " " + bookData["subjects"] + " " + bookData["genres"]
 
-similarity_matrix = linear_kernel(overview_matrix,overview_matrix)
 
-#books index mapping
-mapping = pd.Series(bookData.index,index = bookData["isbn13"])
+def get_similarity_matrix(bookData, tfidf=tfidf):
+    tfidf_matrix = tfidf.fit_transform(bookData["book_info"])
+    similarity_matrix = linear_kernel(tfidf_matrix,tfidf_matrix)
+    return similarity_matrix
+
+similarities = []
+mappings = []
+for i in range(0, CHUNK_QUANTITY):
+    len_books = bookData.shape[0] / CHUNK_QUANTITY
+    chunkData = bookData[int(i*len_books):int((i+1)*len_books)]
+    chunkData = chunkData.reset_index(drop=True)
+    similarity_matrix = get_similarity_matrix(chunkData)
+    similarities.append(similarity_matrix)
+    mappings.append(pd.Series(chunkData.index,index = chunkData["isbn13"]))
+
 
 def recommend_tf_idf(isbn13):
-    book_index = mapping[isbn13]
-    #get similarity values with other movies
-    #similarity_score is the list of index and similarity matrix
-    similarity_score = list(enumerate(similarity_matrix[book_index]))
-    #sort in descending order the similarity score of movie inputted with all the other movies
-    similarity_score = sorted(similarity_score, key=lambda x: x[1], reverse=True)
-    # Get the scores of the 15 most similar movies. Ignore the first movie.
-    similarity_score = similarity_score[1:15]
-    #return movie names using the mapping series
-    book_indices = [i[0] for i in similarity_score]
-    return (bookData[["book_title", "isbn13", "isbn"]].iloc[book_indices])
+    book_indices = []
+    for i in range(0, CHUNK_QUANTITY):
+        if(isbn13 in mappings[i].index):
+            similarity_matrix = similarities[i]
+            book_index = mappings[i][isbn13]
+            if(isinstance(book_index,pd.Series)):
+                book_index = book_index[0]
+            similarity_score = list(enumerate(similarity_matrix[book_index]))
+            # Sort the books based on the similarity scores
+            similarity_score = sorted(similarity_score, key=lambda x: x[1], reverse=True)
+            similarity_score = similarity_score[1:15]
+            book_indices.append([i[0] for i in similarity_score])
 
-print(recommend_tf_idf("9780002005012"))
+    print("Book Indices")
+    print(book_indices)
+    return (bookData[["book_title", "isbn13", "isbn"]].iloc[book_indices[0]])
 
-print(bookData.isbn13.head())
+
+#in mapping 3
+print(recommend_tf_idf("9780771074677"))
+
+print(bookData.isbn13.head()) 
+
+book_index = mappings[0]["9780393045215"]
+print(mappings[1])
+print(book_index)
+book_index = mappings[1]["9780553580334"]
+print(type(book_index))
+
+print(mappings[1])
